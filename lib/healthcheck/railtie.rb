a49bdb1cdabf771e1d8820cecab078fb09f8e96a
Justin Lambert <jlambert@eml.cc>
2016-11-08 04:12:02 -0700

    if force_ssl insert before ActionDispatch::SSL

--- lib/healthcheck/railtie.rb
+++ lib/healthcheck/railtie.rb
@@ -2,6 +2,10 @@ require 'rails'
 
 module Healthcheck
   class Railtie < ::Rails::Railtie
-    config.app_middleware.use Healthcheck::Middleware
+    if config.force_ssl
+      config.app_middleware.insert_before ActionDispatch::SSL, Healthcheck::Middleware
+    else
+      config.app_middleware.use Healthcheck::Middleware
+    end
   end
 end
